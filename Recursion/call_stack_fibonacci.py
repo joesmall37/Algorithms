f5857eb4b4dbfdd90d509a4eb21a74c81ed0b0d2
Joe Small <small.joseph@outlook.com>
2021-06-18 22:35:29 -0400

    new files

---
+++ Recursion/call_stack_fibonacci.py
@@ -0,0 +1,15 @@
+# define the fibonacci() function below...
+
+def fibonacci(n):
+  # base cases
+  if n == 1:
+    return n
+  if n == 0:
+    return n
+
+  # recursive step
+  print("Recursive call with {0} as input".format(n))
+  return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+fibonacci(5)
