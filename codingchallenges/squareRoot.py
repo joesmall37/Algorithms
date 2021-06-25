676ab74e12e363a5dcbeedb590708c9ff95c8e63
Joe Small <small.joseph@outlook.com>
2021-06-24 23:47:27 -0400

    added coding challenges

---
+++ codingchallenges/squareRoot.py
@@ -0,0 +1,5 @@
+def square_root(num):
+    root = num ** 0.5
+    return root
+
+print(square_root(10))
---
+++ codingchallenges/tenthPower.py
@@ -0,0 +1,9 @@
+# Write a function named tenth_power() that has one parameter named num.
+
+# The function should return num raised to the 10th power.
+
+def tenth_power(num):
+    power_ten = num ** 10
+    return power_ten
+
+print(tenth_power(10))
