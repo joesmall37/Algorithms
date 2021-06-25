9b3e8ac563b50043b83f7ad26251896ac5dc42e5
Joe Small <small.joseph@outlook.com>
2021-06-24 22:28:54 -0400

    completed mergesort

---
+++ MergeSort/baseCase.py
@@ -0,0 +1,8 @@
+# check if the length of the items of the list is 1 or less
+def merge_sort(items):
+  if len(items) <= 1:
+    return items
+
+
+
+
---
+++ MergeSort/mergeFunction.py
@@ -0,0 +1,5 @@
+# create the intial merge function to reassemble our data
+
+def merge(left, right):
+  result = []
+  return result
