6a8860b6289040f173718f24bbfa0c83ef1a536b
Joe Small <small.joseph@outlook.com>
2021-06-29 00:11:26 -0400

    lists

---
+++ DataStructures/Lists/lists.py
@@ -0,0 +1,14 @@
+data_science_topics = ["Machine Learning", "SQL", "Pandas", "Algorithms", "Statistics", "Python 3"]
+print(data_science_topics)
+
+
+
+data_science_topics.pop()
+
+print(data_science_topics)
+
+data_science_topics.pop(3)
+
+print(data_science_topics)
+
+# by default, pop will remove the last index, however, if we specific, it can remove a specific index
